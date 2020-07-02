628b3c57a8086cc0abc57a4ed38c8111d6337216
Anna Moeller <anna.moeller@umontana.edu>
2020-07-02 14:27:57 -0600

    Change in develop branch

--- First script.R
+++ First script.R
@@ -1,3 +1,4 @@
 # This is my first script
 
-# I'd like to test tagging a version number 
+# I'd like to test tagging a version number 0.0.1
+# I'm adding code to my develop branch. I don't want this line to show up in master
